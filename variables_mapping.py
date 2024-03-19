7ea55ebe4c07e567f467eec489fb30a90a234393
aksisonline <teamscientify2016@gmail.com>
2024-03-20 00:21:56 +0530

    created an env

--- variables_mapping.py
+++
@@ -1,8 +0,0 @@
-# variables_mapping.py
-
-column_mapping = {
-    'name': 'recipient_name',
-    'email': 'recipient_email',
-    'cnf_id': 'recipient_txn_id',
-    'acc_type': 'recipient_acc_type'
-}
